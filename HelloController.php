f9209bbf7c7b3e5e898a1e22b81238cc0c1e0720
upbook26 <upbook.26@gmail.com>
2020-09-30 22:21:15 +0900

    commit

---
+++ HelloController.php
@@ -0,0 +1,20 @@
+app http controllers  
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class HelloController extends Controller
+{
+    public function index()
+    {
+        return view('hello.index');
+    }
+    public function post(Request $request)
+    {
+        return view('hello.index',['msg'=>$request->msg]);
+    }
+
+}
